db330fc819fe45fea1b550fe0ab4945f279ae2b3
Christopher Michel <christopher.michel@edu.pucrs.br>
2023-09-23 17:21:28 -0300

    Primeira versao paralela

--- sum.c
+++ sum.c
@@ -19,30 +19,32 @@ void sum(char* output, const long unsigned int d, const long unsigned int n) {
     for (digit = 0; digit < d + 11; ++digit) {
         digits[digit] = 0;
     }
+    
+    #pragma omp parallel for private(i, remainder, div, mod) shared(digits)
     for (i = 1; i <= n; ++i) {
         remainder = 1;
         for (digit = 0; digit < d + 11 && remainder; ++digit) {
             div = remainder / i;
             mod = remainder % i;
+            #pragma omp atomic
             digits[digit] += div;
             remainder = mod * 10;
         }
     }
+
     for (i = d + 11 - 1; i > 0; --i) {
         digits[i - 1] += digits[i] / 10;
         digits[i] %= 10;
     }
+
     if (digits[d + 1] >= 5) {
         ++digits[d];
     }
-    for (i = d; i > 0; --i) {
-        digits[i - 1] += digits[i] / 10;
-        digits[i] %= 10;
-    }
-    sprintf(output,"%lu.",digits[0]);
+
+    sprintf(output, "%lu.", digits[0]);
     unsigned long int t = strlen(output);
     for (i = 1; i <= d; ++i)
-        output[t++] = digits[i]+'0';
+        output[t++] = digits[i] + '0';
     output[t] = '\0';
 }
 
